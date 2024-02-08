ec9627ad1b661cfc45e04c5ef10a7a85a23b6ac0
Andreas Söderlund <ciscoheat@gmail.com>
2024-02-08 07:34:47 +0100

    Test failing in browser with ERR_SVELTE_HYDRATION_MISMATCH

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Log from './Log.svelte';
+	let output: (string[] | undefined)[];
+</script>
+
+<Log bind:output />
+
+<!-- Removing Output: makes it work -->
+<pre>
+Output: {#if output}{output.join('\n')}{/if}
+</pre>
---
+++ src/routes/Log.svelte
@@ -0,0 +1,3 @@
+<script lang="ts">
+	export let output: (string[] | undefined)[] = [];
+</script>
